6d810b9570e59cd0e94b6f687e69507980ebbcd1
madhav <madhavsaibhushan@gmail.com>
2019-12-25 18:35:03 +0530

    deleted bootstrap installed   wrong

--- attendance-registry/src/app/home/home.page.ts
+++ attendance-registry/src/app/home/home.page.ts
@@ -25,7 +25,7 @@ export class HomePage  {
         ImagePath: 'check-in.svg',
         url:'settings'
       }, {
-        title: "Check Attendance",
+        title: "Hr Register",
         ImagePath: 'check-in.svg',
         url:'attendance'
       }
